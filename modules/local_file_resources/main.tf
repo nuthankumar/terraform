ce034c79e30fffb330141f064d952996fcbd8155
Nuthan <nuthankumarkb@gmail.com>
2020-07-10 15:38:24 +0530

    map_test

--- modules/local_file_resources/main.tf
+++ modules/local_file_resources/main.tf
@@ -2,14 +2,14 @@ resource "local_file" "preconfig_file" {
   content  = "${join("", data.template_file.preconfig_privatelan.*.rendered)}"
   filename = "ansible/preconfig/preconfig.yml"
 }
-resource "null_resource" "map_test" {
+/*resource "null_resource" "map_test" {
   count            = length(var.map_test)
   provisioner "local-exec" {
     command = "mkdir ${var.map_test[count.index%3]}"
   }
-  /*  provisioner "local-exec" {
+    provisioner "local-exec" {
      when = "destroy"
      command = "cleanup.bat"
     }
-    */
-}
+    
+}*/
